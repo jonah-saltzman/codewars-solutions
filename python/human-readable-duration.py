602c072a27b00a859fb6619aeaa08dea8a0ef831
jonahsaltzman <jonahsaltzman@gmail.com>
2021-11-10 13:29:19 -0500

    human-readable-duration: passes manual tests

--- python/human-readable-duration.py
+++ python/human-readable-duration.py
@@ -1,6 +1,9 @@
 def main():
-    time = 3662
-    print(format_duration(time))
+    print(format_duration(1))
+    print(format_duration(62))
+    print(format_duration(120))
+    print(format_duration(3600))
+    print(format_duration(3662))
     return
 
 units = {
@@ -21,16 +24,16 @@ def format_duration(time):
     }
 
     while time != 0:
-        if time - units['year'] > 0:
+        if time - units['year'] >= 0:
             time = time - units['year']
             results['year'] += 1
-        elif time - units['day'] > 0:
+        elif time - units['day'] >= 0:
             time = time - units['day']
             results['day'] += 1
-        elif time - units['hour'] > 0:
+        elif time - units['hour'] >= 0:
             time = time - units['hour']
             results['hour'] += 1
-        elif time - units['minute'] > 0:
+        elif time - units['minute'] >= 0:
             time = time - units['minute']
             results['minute'] += 1
         else:
@@ -51,7 +54,25 @@ def format_duration(time):
     for element in results:
         strings.append(formatUnit(element, results[element]))
 
-    return strings
+    separatedStrings = []
+    for unit in strings:
+        if unit == strings[0]:
+            unitCount -= 1
+            tempStr = unit
+        else:
+            if unitCount >= 2:
+                tempStr = ", " + unit
+                unitCount -= 1
+            elif unitCount == 1:
+                tempStr = " and " + unit
+                unitCount -= 1
+        separatedStrings.append(tempStr)
+    
+    resultStr = ""
+    for string in separatedStrings:
+        resultStr += string
+
+    return resultStr
 
 def formatUnit(unit, n):
     string = str(n) + " " + unit
