0389857fecbb93206c7eadd8241db8720eac8cc6
jonahsaltzman <jonahsaltzman@gmail.com>
2021-11-10 11:56:39 -0500

    merge branch back into main

---
+++ python/petethebaker.py
@@ -0,0 +1,21 @@
+def main():
+    recipe = {
+        'flour': 500,
+        'sugar': 200,
+        'eggs': 1
+        }
+
+    ingredients = {
+        'flour': 1200,
+        'sugar': 1200,
+        'eggs': 5,
+        'milk': 200
+        }
+    print(recipe)
+    print(ingredients)
+
+def cakes(recipe, available):
+    
+
+if __name__ == "__main__":
+    main()
