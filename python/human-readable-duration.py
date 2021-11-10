1f26d935781bf05b0e7f7347667cb2b79e4f0d58
jonahsaltzman <jonahsaltzman@gmail.com>
2021-11-10 13:54:29 -0500

    human-readable-duration: final refactor

--- python/human-readable-duration.py
+++ python/human-readable-duration.py
@@ -1,11 +1,7 @@
-def main():
-    print(format_duration(1))
-    print(format_duration(62))
-    print(format_duration(120))
-    print(format_duration(3600))
-    print(format_duration(3662))
-    return
+# python | 'human readable duration format' | 4kyu
+# codewars url: https://www.codewars.com/kata/52742f58faf5485cae000b9a/
 
+# dict containing unit values in seconds
 units = {
     'year': 31536000,
     'day': 86400,
@@ -14,7 +10,10 @@ units = {
     'second': 1
 }
 
+# main function
 def format_duration(time):
+
+    # dict for tallying number of each unit in 'time'
     results = {
         'year': 0,
         'day': 0,
@@ -23,9 +22,12 @@ def format_duration(time):
         'second': 0
     }
 
+    # catch zero/empty arguments
     if time in (0, ''):
         return 'now'
 
+    # subtract largest possible unit (in seconds) from time
+    # until remaining time is 0
     while time != 0:
         if time - units['year'] >= 0:
             time = time - units['year']
@@ -43,6 +45,8 @@ def format_duration(time):
             time = time - units['second']
             results['second'] += 1
 
+    # count number of non-zero units and remove all others
+    # from 'results' dict
     unitCount = 0
     pop = []
     for unit in results:
@@ -53,10 +57,14 @@ def format_duration(time):
     for unit in pop:
         results.pop(unit)
     
+    # generate list 'strings' containing properly-formatted
+    # human-readable strings (no separators yet)
     strings = []
     for element in results:
         strings.append(formatUnit(element, results[element]))
 
+    # add appropriate separators to strings in 'strings' according to
+    # number of unedited strings remaining; add new string to 'separatedStrings'
     separatedStrings = []
     for unit in strings:
         if unit == strings[0]:
@@ -71,18 +79,18 @@ def format_duration(time):
                 unitCount -= 1
         separatedStrings.append(tempStr)
     
+    # combine all strings in list 'separatedStrings' into
+    # single string, and return that string
     resultStr = ""
     for string in separatedStrings:
         resultStr += string
-
     return resultStr
 
+# given a name of unit (string) and quantity, generate 
+# full human-readable string containing unit & quantity
+# add s for plural units
 def formatUnit(unit, n):
     string = str(n) + " " + unit
     if n == 1:
         return string
     return string + "s"
-
-
-if __name__ == "__main__":
-    main()
