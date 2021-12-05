eccc00fa9e1138bf7317a344fd464e748129b4e8
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-05 02:09:22 -0500

    clean up tic-tac-toe-solver.js

--- tic-tac-toe-prototype/tic-tac-toe-solver.js
+++ tic-tac-toe-prototype/tic-tac-toe-solver.js
@@ -1,62 +1,74 @@
-
 function movesToCompare(board, player) {
+	const moves = []
+	getMoves(board, player).forEach((move) => moves.push({ move: move }))
 
-    const moves = [] 
-    getMoves(board, player).forEach(move => moves.push({move: move}))
+	moves.forEach(
+		(move) =>
+			(move.score = scoreBoard(generateBoard(board, move.move), player, player))
+	)
 
-    moves.forEach(move => move.score = scoreBoard(generateBoard(board, move.move), player, player))
+	const validMoves = moves.filter((score) => score.move.player)
+	const highestScore = validMoves.reduce(
+		(highScore, move) => (move.score > highScore ? move.score : highScore),
+		validMoves[0].score
+	)
 
-    const validMoves = moves.filter((score) => score.move.player)
-    const highestScore = validMoves.reduce(
-        (highScore, move) => (move.score > highScore ? move.score : highScore),
-        validMoves[0].score
-    )
-
-    return validMoves.filter(move => move.score === highestScore)
+	return validMoves.filter((move) => move.score === highestScore)
 }
 
 function scoreBoard(board, moveBy, player) {
+	const boardResult = gameOver(board, player)
+	if (boardResult !== false) return boardResult
 
-    const boardResult = gameOver(board, player)
-    if (boardResult !== false) return boardResult
+	const nextMover = moveBy === 'x' ? 'o' : 'x'
 
-    const nextMover = moveBy === 'x' ? 'o' : 'x'
+	const boards = getMoves(board, nextMover).map((move) =>
+		move.player ? generateBoard(board, move) : null
+	)
 
-    const boards = getMoves(board, nextMover).map(move => move.player ? generateBoard(board, move) : null)
-
-    return boards.reduce((score, board) => board ? score + scoreBoard(board, nextMover, player) : score, 0)
+	return boards.reduce(
+		(score, board) =>
+			board ? score + scoreBoard(board, nextMover, player) : score,
+		0
+	)
 }
 
 function generateBoard(board, move) {
-    if (move.player === null) return null
-    const newBoard = [...board]
-    newBoard[move.toIndex] = move.player
-    return newBoard
+	if (move.player === null) return null
+	const newBoard = [...board]
+	newBoard[move.toIndex] = move.player
+	return newBoard
 }
 
 function getMoves(board, nextPlayer) {
-    const moves = [] 
-    board.forEach((position, index) =>
-        moves.push({
-            player: position ? null : nextPlayer,
-            toIndex: index,
-        })
-    )
-    return moves
+	const moves = []
+	board.forEach((position, index) =>
+		moves.push({
+			player: position ? null : nextPlayer,
+			toIndex: index,
+		})
+	)
+	return moves
 }
 
 const gameOver = (board, player) => {
 	const opponent = player === 'x' ? 'o' : 'x'
 	if (board === null) return 0
-	if (conditions.some((condition) =>
-        condition.every((pos) => board[pos] === player))) {
-            wins++
-            return 1
+	if (
+		conditions.some((condition) =>
+			condition.every((pos) => board[pos] === player)
+		)
+	) {
+		wins++
+		return 1
 	}
-	if (conditions.some((condition) =>
-		condition.every((pos) => board[pos] === opponent))) {
-            losses++
-            return -1
+	if (
+		conditions.some((condition) =>
+			condition.every((pos) => board[pos] === opponent)
+		)
+	) {
+		losses++
+		return -1
 	}
 	if (board.every((pos) => pos)) {
 		draws++
